cf84e1efd9f507ca5fd748b6238e729f3a0bdb5c
rsabbah <rpasabb@gmail.com>
2023-11-22 16:59:20 +0100

    all working

--- srcs/requirements/bonus/ftp/tools/entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/entrypoint.sh
@@ -5,17 +5,20 @@ chpasswd <<< ftpuser:${FTP_PASSWORD}
 
 echo ftpuser >> /etc/vsftpd.userlist
 
+chown ftpuser /home/ftpuser/ftp/files
+
 #uncomment to allow write operations (upload)
 sed -i 's/#write_enable=YES/write_enable=YES/g' /etc/vsftpd.conf
+sed -i 's/#chroot_local_user=YES/chroot_local_user=YES/g' /etc/vsftpd.conf
 
 #add config options to config file
 cat <<- EOF >> /etc/vsftpd.conf
 	local_enable=YES
 	allow_writeable_chroot=YES
 	pasv_enable=YES
-	local_root=/var/www/html
-	pasv_min_port=40000
-	pasv_max_port=40005
+	local_root=/home/ftpuser/ftp
+	pasv_min_port=10000
+	pasv_max_port=10010
 	userlist_file=/etc/vsftpd.userlist
 EOF
 
